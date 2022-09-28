8c1d3a182e1edce0cd5f0b1da6908598a7aad8b8
Zachary Sarette <zacharywsarette@gmail.com>
2022-09-28 19:14:52 +0900

    revised multi line f

--- basics/multi_line_f_strings.py
+++ basics/multi_line_f_strings.py
@@ -1,11 +1,13 @@
 # multi line f string example
 
-name = input("What is your name?")
-name = input("What is a feature that you don't like about Python?")
+name = input("What is your name?\n")
+
+feature = input("What is a feature that you don't like about Python?\n")
 
 print(
-    f"Hello my name is {name}. "
-        "I like Python."
-    f"But I don't like {} in Python. "
-    f""
+    f'''Hello my name is {name}. 
+I like Python. 
+But I don't like {feature} in Python. 
+
+'''
 )
